c52c802e5a9c02301ef046fa52e22528cf71c6fd
Seung Jae Yang <sjyang1126@gmail.com>
2024-09-25 10:05:39 -0400

    Example 2-2: dynamic route

--- hello.py
+++ hello.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+# if __name__ == '__main__':
+#     app.run()
